2f012d24555f141e0d3e5e1578f76f5444cf6099
wellingtonbarbosadev <106507628+wellingtonbarbosadev@users.noreply.github.com>
2026-04-24 13:30:17 -0300

    Adicionar suporte a tratamento de erros assíncronos com express-async-errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import "express-async-errors"
+
+import { errorHandling } from './middlewares/error-handling';
 
 const app = express();
+
 app.use(express.json());
+
+app.use(errorHandling)
 
 export { app }
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -8,6 +8,8 @@ export function errorHandling(
   next: NextFunction
 ) {
   if (error instanceof AppError) {
-    
+    return response.status(error.statusCode).json({ message: error.message})
   }
+
+  return response.status(500).json({ message: error.message })
 }
