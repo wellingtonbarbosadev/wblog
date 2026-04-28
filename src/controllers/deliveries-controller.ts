f956bb7ba6df2bab3bdb1d53e07e5b2a6d7419d6
wellingtonbarbosadev <106507628+wellingtonbarbosadev@users.noreply.github.com>
2026-04-28 14:35:22 -0300

    implementar criação de entregas com validação de dados usando Zod

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,12 +1,28 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import z from "zod";
 
 class DeliveriesController {
   index(request: Request, response: Response, next: NextFunction) {
     return response.json();
   }
 
-  create(request: Request, response: Response, next: NextFunction) {
-    return response.json("OK");
+  async create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = z.object({
+      user_id: z.uuid(),
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
 
