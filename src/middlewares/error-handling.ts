4e3f0087797fc85f3a6d70a688df9f8ab9578b3a
Wellington Barbosa <huehueb58@gmail.com>
2026-04-29 23:40:07 -0300

    corrigir formatação e melhorar a manipulação de erros no middleware de tratamento de erros

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -6,18 +6,18 @@ export function errorHandling(
   error: any,
   request: Request,
   response: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   if (error instanceof AppError) {
-    return response.status(error.statusCode).json({ message: error.message})
+    return response.status(error.statusCode).json({ message: error.message });
   }
 
   if (error instanceof ZodError) {
     return response.status(400).json({
       message: "validation error",
-      issues: z.prettifyError(error)
-    })
+      issues: z.flattenError(error),
+    });
   }
 
-  return response.status(500).json({ message: error.message })
+  return response.status(500).json({ message: error.message });
 }
